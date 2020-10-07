42238a9e9141ce8f3e4d715f750db83a83f0f078
vvkoko <45474947+vvkoko@users.noreply.github.com>
2020-10-07 11:13:01 +1100

    Update plot4.R

--- 4_Exploratory_data_analysis/data/plot4.R
+++ 4_Exploratory_data_analysis/data/plot4.R
@@ -16,7 +16,7 @@ powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S"
 # Filter Dates for 2007-02-01 and 2007-02-02
 powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
-png("plot4.png", width=600, height=600)
+png("plot4.png", width=480, height=480)
 
 par(mfrow=c(2,2))
 
